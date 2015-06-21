5dcac54f2536f144acc60040d48c8e33e0527c33
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-06-21 19:30:06 +0300

    Fixes to example to show that $scope.getReactively works on $rootScope as well

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-Userinfo = new Mongo.Collection("userinfo");
-
-Userinfo.allow({
+Meteor.users.allow({
     insert: function (userId, party) {
         return true;
     },
@@ -16,50 +14,24 @@ if (Meteor.isClient) {
 
     angular.module('socially', ['angular-meteor']);
 
-    angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor', '$rootScope', '$log',
-        function ($scope, $meteor, $rootScope, $log) {
+    angular.module("socially").controller("PartiesListCtrl", ['$scope',
+        function ($scope) {
 
-            $scope.userinfo = $scope.$meteorCollection(Userinfo).subscribe('userinfo');
-            $meteor.autorun($scope, function() {
-                if (null !== $rootScope.currentUser && undefined !== $rootScope.currentUser) {
-                    $log.debug('user - ' + $rootScope.currentUser);
-                    $scope.userForScore = $scope.$meteorObject(Userinfo, {user_id: $scope.getReactively('currentUser')._id});//$rootScope.currentUser._id
-                }
+            $scope.userinfo = $scope.$meteorCollection(Meteor.users);
+
+            $scope.$meteorAutorun(function(){
+                if ($scope.getReactively('currentUser'))
+                    $scope.userForScore = $scope.$meteorObject(Meteor.users, {_id: $scope.getReactively('currentUser')._id}, false);
             });
 
-            $scope.userScore = function(){
-                if ($scope.userForScore === undefined || $scope.userForScore.score === undefined) {
-                    return 0;
-                }
-                return $scope.userForScore.score;
-            };
             $scope.updateScore = function(){
                 var score = Math.floor((Math.random() * 100) + 1);
-                $scope.userinfo.save({user_id: $rootScope.currentUser._id, score: score});
+                if (!$scope.userForScore.profile)
+                    $scope.userForScore.profile = {};
+
+                $scope.userForScore.profile.score = score;
+                $scope.userForScore.save();
             };
 
         }]);
-}
-
-if (Meteor.isServer) {
-    Meteor.startup(function () {
-        if (Userinfo.find().count() === 0) {
-            //var parties = [
-            //    {
-            //        'name': 'Dubstep-Free Zone',
-            //        'description': 'Fast just got faster with Nexus S.'
-            //    },
-            //    {
-            //        'name': 'All dubstep all the time',
-            //        'description': 'Get it on!'
-            //    },
-            //    {
-            //        'name': 'Savage lounging',
-            //        'description': 'Leisure suit required. And only fiercest manners.'
-            //    }
-            //];
-            //for (var i = 0; i < parties.length; i++)
-            //    Userinfo.insert(parties[i]);
-        }
-    });
 }
